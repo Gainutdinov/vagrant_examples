57e835f2812890a3136185925b0b39db4c9a3732
mgainutdinov <m.gaynutdinov@mail.ru>
2022-05-24 22:39:55 +0300

    Fix

---
+++ rabbitmq/RabbitMQ-In-Depth/build.sh
@@ -0,0 +1,4 @@
+docker-compose down && docker-compose up --build -d
+docker-compose ps
+#docker exec -ti notebook_InDepth bash
+#  ipython notebook --port=8888 --no-browser --ip='*' --allow-root --NotebookApp.token='' --NotebookApp.password=''

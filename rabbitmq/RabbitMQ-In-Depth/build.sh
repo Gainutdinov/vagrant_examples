a7471eae983f1be634627a4a00f9cf6f2d67bba6
atlas <atlas@MacBook-Pro-atlas.local>
2022-06-08 18:54:17 +0300

    Fix

--- rabbitmq/RabbitMQ-In-Depth/build.sh
+++ rabbitmq/RabbitMQ-In-Depth/build.sh
@@ -2,3 +2,4 @@ docker-compose down && docker-compose up --build -d
 docker-compose ps
 #docker exec -ti notebook_InDepth bash
 #  ipython notebook --port=8888 --no-browser --ip='*' --allow-root --NotebookApp.token='' --NotebookApp.password=''
+docker exec notebook_InDepth bash  -c "ipython notebook --port=8888 --no-browser --ip='*' --allow-root --NotebookApp.token='' --NotebookApp.password=''"
--- rabbitmq/RabbitMQ-In-Depth/python.Dockerfile
+++ rabbitmq/RabbitMQ-In-Depth/python.Dockerfile
@@ -2,4 +2,5 @@ FROM python:3.6.10-buster
 WORKDIR /notebooks
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
+RUN apt update && apt install libgl1 -y
 EXPOSE 8888 8883

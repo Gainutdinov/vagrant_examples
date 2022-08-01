3123eb16e3e375215f7cfd9c4cc89cb458370271
atlas <atlas@MacBook-Pro-atlas.local>
2022-08-01 08:49:53 +0300

    fix

--- learning_postgresql/PGSQL_14:Final_task/parse_inventory.sh
+++ learning_postgresql/PGSQL_14:Final_task/parse_inventory.sh
@@ -26,8 +26,8 @@ cat "$(echo $PWD)/hosts.txt" | while read line; do
     etcd*|pgbouncer*|pgsql-*|haproxy*)
       ansible_host=$(echo "$line" | cut -d '@' -f2 | cut -d ':' -f1 | cut -d ' ' -f1)
       ansible_password=$(echo "$line"  | cut -d ':' -f4)
-      inventory_name=$(echo "$line"  | cut -d ' ' -f1)
-      addtofile "$(echo "${inventory_name::-1} ansible_user=user ansible_host=${ansible_host} ansible_password=${ansible_password:1}" )"
+      inventory_name=$(echo "$line"  | cut -d ':' -f1)
+      addtofile "$(echo "${inventory_name} ansible_user=user ansible_host=${ansible_host} ansible_password=${ansible_password:1}" )"
       ;;
   esac
 
